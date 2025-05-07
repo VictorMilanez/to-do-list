185202cd76e94a5b87963e9298c385b3cf1e9266
Victor Milanez <victorsilva183@gmail.com>
2025-05-07 14:21:52 -0300

    Adicionados botões para deletar, visualizar ativas, completadas e limpar completadas.

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -12,6 +12,8 @@ export const TasksList = () => {
       ...list,
       { id: list.length + 1, text: itemInput, checked: false },
     ]);
+
+    setItemInput("");
   };
 
   return (
@@ -35,17 +37,37 @@ export const TasksList = () => {
         />
       </div>
       <div className="w-2/5 h-3/5 flex flex-col items- bg-[hsl(235,24%,19%)] absolute top-2/7 left-[30%] rounded-md">
-        <div>
+        <div className="overflow-y-auto flex-1">
           {list.map((item) => (
             <li
               key={item.id}
-              className="p-6 text-[hsl(234,39%,85%)] text-xl border-b-2 border-[hsl(237,14%,26%)]"
+              className="flex items-center gap-4 py-6 text-[hsl(234,39%,85%)] text-xl border-b-2 border-[hsl(237,14%,26%)] last:border-b-0 list-none cursor-pointer"
             >
+              <div className="w-8 h-8 flex items-center justify-center ml-4 border-2 border-[hsl(237,14%,26%)] rounded-full cursor-pointer hover:border-[hsl(237,14%,26%)] hover:border-2 hover:[background:linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))] hover:bg-clip-border">
+                <div className="w-6 h-6 rounded-full bg-[hsl(235,24%,19%)]"></div>
+              </div>
               {item.text}
+              <button className="ml-auto pr-6 text-3xl text-[hsl(234,11%,52%)] font-light cursor-pointer">
+                X
+              </button>
             </li>
           ))}
         </div>
-        <div>...</div>
+        <div className="flex justify-between py-4 pl-6 h-16 border-t-2 border-[hsl(237,14%,26%)]">
+          <p className="text-[hsl(234,11%,52%)]">4 itens left</p>
+          <div className="flex gap-4">
+            <button className="text-[hsl(220,98%,61%)]">All</button>
+            <button className="text-[hsl(234,11%,52%)] hover:[color:hsl(236,33%,92%)] cursor-pointer">
+              Active
+            </button>
+            <button className="text-[hsl(234,11%,52%)] hover:[color:hsl(236,33%,92%)] cursor-pointer">
+              Completed
+            </button>
+          </div>
+          <button className="text-[hsl(234,11%,52%)] pr-6 hover:[color:hsl(236,33%,92%)] cursor-pointer">
+            Clear Completed
+          </button>
+        </div>
       </div>
     </>
   );

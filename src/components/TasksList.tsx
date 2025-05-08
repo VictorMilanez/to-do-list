e6c4e538c1c5c80462a2dbdb960ab91138aee779
Victor Milanez <victorsilva183@gmail.com>
2025-05-08 11:04:59 -0300

    Adicionado as lógicas de mostrar todas tarefas, apenas as tarefas finalizadas , apenas as tarefas pendente e de limpar todas as tarefas finalizadas.

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -6,6 +6,9 @@ import iconDelete from "../../public/images/icon-cross.svg";
 export const TasksList = () => {
   const [itemInput, setItemInput] = useState("");
   const [list, setList] = useState<List[]>([]);
+  const [listCompleted, setListCompleted] = useState<
+    "all" | "active" | "completed"
+  >("all");
 
   const handleAddItem = () => {
     if (itemInput.trim() === "") return;
@@ -30,6 +33,10 @@ export const TasksList = () => {
     );
   };
 
+  const clearCompletedItems = () => {
+    setList(list.filter((item) => !item.checked));
+  };
+
   return (
     <>
       <div className="w-2/5 h-20 flex items-center bg-[hsl(235,24%,19%)] absolute top-1/6 left-[30%] rounded-md">
@@ -52,51 +59,78 @@ export const TasksList = () => {
       </div>
       <div className="w-2/5 h-3/5 flex flex-col items- bg-[hsl(235,24%,19%)] absolute top-2/7 left-[30%] rounded-md">
         <div className="overflow-y-auto flex-1">
-          {list.map((item) => (
-            <li
-              key={item.id}
-              className="flex items-center gap-4 py-6 text-[hsl(234,39%,85%)] text-xl border-b-2 border-[hsl(237,14%,26%)] last:border-b-0 list-none cursor-pointer"
-            >
-              <div
-                onClick={() => toggleItem(item.id)}
-                className={`w-8 h-8 flex items-center justify-center ml-4 border-2 border-[hsl(237,14%,26%)] rounded-full cursor-pointer hover:border-[hsl(237,14%,26%)] hover:border-2 hover:[background:linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))] hover:bg-clip-border ${
-                  item.checked
-                    ? "[background:linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))]"
-                    : "border-[hsl(237,14%,26%)]"
+          {list
+            .filter((item) => {
+              if (listCompleted === "completed") return item.checked;
+              if (listCompleted === "active") return !item.checked;
+              return true;
+            })
+            .map((item) => (
+              <li
+                key={item.id}
+                className={`flex items-center gap-4 py-6 text-[hsl(234,39%,85%)] text-xl border-b-2 border-[hsl(237,14%,26%)] last:border-b-0 list-none cursor-pointer ${
+                  item.checked ? "line-through" : ""
                 }`}
+                style={{
+                  color: item.checked
+                    ? "hsl(234, 11%, 52%)"
+                    : "hsl(234,39%,85%)",
+                }}
               >
-                {item.checked ? (
-                  <img
-                    src={iconChecked}
-                    alt="ícone check"
-                    className="w-5 h-5"
-                  />
-                ) : (
-                  <div className="w-6 h-6 rounded-full bg-[hsl(235,24%,19%)]"></div>
-                )}
-              </div>
-              {item.text}
-              <button
-                onClick={() => handleDeleteItem(item.id)}
-                className="ml-auto pr-6 text-3xl text-[hsl(234,11%,52%)] font-light cursor-pointer"
-              >
-                <img src={iconDelete} alt="ícone para deletar tarefa" />
-              </button>
-            </li>
-          ))}
+                <div
+                  onClick={() => toggleItem(item.id)}
+                  className={`w-8 h-8 flex items-center justify-center ml-4 border-2 border-[hsl(237,14%,26%)] rounded-full cursor-pointer hover:border-[hsl(237,14%,26%)] hover:border-2 hover:[background:linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))] hover:bg-clip-border ${
+                    item.checked
+                      ? "[background:linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))]"
+                      : "border-[hsl(237,14%,26%)]"
+                  }`}
+                >
+                  {item.checked ? (
+                    <img
+                      src={iconChecked}
+                      alt="ícone check"
+                      className="w-5 h-5 pr-0.5"
+                    />
+                  ) : (
+                    <div className="w-7 h-7 rounded-full bg-[hsl(235,24%,19%)]"></div>
+                  )}
+                </div>
+                {item.text}
+                <button
+                  onClick={() => handleDeleteItem(item.id)}
+                  className="ml-auto pr-6 text-3xl text-[hsl(234,11%,52%)] font-light cursor-pointer"
+                >
+                  <img src={iconDelete} alt="ícone para deletar tarefa" />
+                </button>
+              </li>
+            ))}
         </div>
-        <div className="flex justify-between py-4 pl-6 h-16 border-t-2 border-[hsl(237,14%,26%)]">
-          <p className="text-[hsl(234,11%,52%)]">4 itens left</p>
+        <div className="flex justify-between items-center py-4 pl-6 h-22 border-t-2 border-[hsl(237,14%,26%)]">
+          <p className="text-[hsl(234,11%,52%)]">{list.length} items left</p>
           <div className="flex gap-4">
-            <button className="text-[hsl(220,98%,61%)]">All</button>
-            <button className="text-[hsl(234,11%,52%)] hover:[color:hsl(236,33%,92%)] cursor-pointer">
+            <button
+              onClick={() => setListCompleted("all")}
+              className="text-[hsl(220,98%,61%)] cursor-pointer"
+            >
+              All
+            </button>
+            <button
+              onClick={() => setListCompleted("active")}
+              className="text-[hsl(234,11%,52%)] hover:[color:hsl(236,33%,92%)] cursor-pointer"
+            >
               Active
             </button>
-            <button className="text-[hsl(234,11%,52%)] hover:[color:hsl(236,33%,92%)] cursor-pointer">
+            <button
+              onClick={() => setListCompleted("completed")}
+              className="text-[hsl(234,11%,52%)] hover:[color:hsl(236,33%,92%)] cursor-pointer"
+            >
               Completed
             </button>
           </div>
-          <button className="text-[hsl(234,11%,52%)] pr-6 hover:[color:hsl(236,33%,92%)] cursor-pointer">
+          <button
+            onClick={() => clearCompletedItems()}
+            className="text-[hsl(234,11%,52%)] pr-6 hover:[color:hsl(236,33%,92%)] cursor-pointer"
+          >
             Clear Completed
           </button>
         </div>

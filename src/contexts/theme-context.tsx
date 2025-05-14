00ebff83212d4c094bda197b8e6ecb9440131c3b
Victor Milanez <victorsilva183@gmail.com>
2025-05-14 09:59:21 -0300

    Adicionado localStorage para salvar o tema presente.

--- src/contexts/theme-context.tsx
+++ src/contexts/theme-context.tsx
@@ -2,42 +2,20 @@ import { useEffect, useState } from "react";
 import { ThemeType } from "../types/Theme";
 import { ThemeContext as Context, themes } from "./theme-data";
 
-// const themes: Record<ThemeType, ThemeValues> = {
-//   dark: {
-//     bgPrimary: "hsl(235, 24%, 19%)",
-//     border: "hsl(237, 14%, 26%)",
-//     textPrimary: "hsl(234, 39%, 85%)",
-//     textSecondary: "hsl(234, 11%, 52%)",
-//     textHover: "hsl(236, 33%, 92%)",
-//   },
-
-//   light: {
-//     bgPrimary: "hsl(236, 9%, 61%)",
-//     border: "hsl(235, 19%, 35%)",
-//     textPrimary: "hsl(233,11%,84%)",
-//     textSecondary: "hsl(236,9%,61%)",
-//     textHover: "hsl(236, 33%, 92%)",
-//   },
-// };
-
-// export const ThemeContext = createContext<ThemeContextType>({
-//   theme: themes.light,
-//   themeName: "light",
-//   toggleTheme: () => {},
-// });
-
 export const ThemeContext = Context;
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [themeName, setThemeName] = useState<ThemeType>("light");
+  const [themeName, setThemeName] = useState<ThemeType>(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme ? (savedTheme as ThemeType) : "light";
+  });
 
   useEffect(() => {
+    localStorage.setItem("theme", themeName);
     if (themeName === "dark") {
       document.documentElement.classList.add("dark");
-      //document.documentElement.classList.remove("light");
     } else {
       document.documentElement.classList.remove("dark");
-      //document.documentElement.classList.add("light");
     }
   }, [themeName]);
 
